76eae03d15e98a633b89c062b8e8a18c2e975c72
Alexandra Frock <im@alexandra.moe>
2017-06-09 13:42:55 -0400

    Ignore accepted offers (for now)

--- poll.js
+++ poll.js
@@ -15,6 +15,11 @@ module.exports = function(){
       Object.keys(b).forEach(function(v){
         console.log("Discovered offer " + v + " (" + b[v].from_user_hex + " -> " + b[v].to_user_hex + ")");
 
+        if( b[v].to_status != "pending" ){
+          console.log("fixme: This is an accepted offer");
+          return;
+        }
+
         var user = func.getDatabaseEntry(b[v].to_user_hex, database);
 
         if( user != null && ( user.notify || ( user.emailNotify && user.hasOwnProperty("email") ) ) ){
